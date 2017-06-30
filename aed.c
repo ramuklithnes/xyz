b89e2fac408b45a0dbba2a45e89775f313352b1d
ramuklithnes <senthil331999@gmail.com>
2017-06-30 14:05:56 +0530

    Create aed.c

---
+++ aed.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+clrscr();
+printf("enter the value:");
+scanf("%c",&n);
+if((n=='1')||(n=='2')||(n=='3')||(n=='4')||(n=='5')||(n=='6')||(n=='7')||(n=='8')||(n=='9'))
+{
+printf("the %c is alphabet:");
+}
+else
+printf("the %c is not alphabet:");
+getch();
+}
